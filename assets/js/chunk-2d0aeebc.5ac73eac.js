7806b2369f60111af7f174c489dd172c3b1777a5
Pedro <ptata2292@gmail.com>
2020-12-09 23:29:49 -0400

    nuevos cambios y integracion de cms y redireccion

---
+++ assets/js/chunk-2d0aeebc.5ac73eac.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aeebc"],{"0bb6":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],l={name:"IndexBlog"},r=l,a=t("2877"),o=Object(a["a"])(r,u,c,!1,null,null,null);n["default"]=o.exports}}]);
---
+++ assets/js/chunk-2d215fc5.6935a7e7.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d215fc5"],{c112:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},u=[],l={name:"IndexBlog"},r=l,o=t("2877"),a=Object(o["a"])(r,c,u,!1,null,null,null);n["default"]=a.exports}}]);
